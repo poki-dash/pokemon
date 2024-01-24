fa9f0d4815c905eb0b6c9e47a296b8f362d9903b
Brandon Ramos <bramos005@citymail.cuny.edu>
2024-01-24 14:10:03 -0500

    finished pop up except button

--- app/src/main.js
+++ app/src/main.js
@@ -5,10 +5,11 @@ import { renderLibrary, renderPokemonData } from "./render";
 
 const main = async () => {
 await  renderLibrary(document.querySelector("#library"))
-  renderPokemonData("pikachu", document.querySelectorAll(".more-info"));
+  
 
-  document.querySelector(".popup").addEventListener("click",() => {
-    document.querySelector(".popup").style.display = "none"
+  document.querySelector(".popup").addEventListener("click", () => {
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector
   }
   )
 };
