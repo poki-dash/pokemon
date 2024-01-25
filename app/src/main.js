4c2e499c9cd3533c4a333ff9043e1b27ce519164
angelicacc00 <angelicac0721@gmail.com>
2024-01-25 17:22:01 -0500

    search feature done

--- app/src/main.js
+++ app/src/main.js
@@ -4,8 +4,11 @@ import {
   renderPokemonData,
   renderRandomPoke,
   renderBattle,
+  renderSearchPoke,
+  renderFailFetch
 } from "./render";
-import { fetchRandPoke } from "./fetchfunctions";
+import { fetchOnePokemon, fetchSearchPoke } from "./fetchFunctions.js";
+
 
 const main = async () => {
   await renderLibrary(document.querySelector("#library"));
@@ -23,12 +26,24 @@ const main = async () => {
     e.target.parentNode.removeChild(e.target);
   });
 
-    document.querySelector("#battle-button").addEventListener("click", (e) => {
+  document.querySelector("#battle-button").addEventListener("click", (e) => {
       renderBattle();
-    });
+  });
   
-
-  
+  const form = document.querySelector('#search');
+  form.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const pokeName = formData.get('pokeName');
+    const pokemonData = await fetchSearchPoke(pokeName);
+    const renderDiv = document.querySelector('#render-search')
+    if(!pokemonData){
+      renderFailFetch(renderDiv)
+    } else {
+      await renderSearchPoke(renderDiv, pokemonData);
+    }
+    e.target.reset();
+  })
 }
 
 main();
